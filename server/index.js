44c8a67c60477abdc89d108f1957ff5722ce636e
nhuquynh <nhuquynhnguyenthi709@gmail.com>
2026-04-09 01:24:47 +0700

    Fix

--- server/index.js
+++ server/index.js
@@ -1,9 +1,17 @@
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
+const http = require('http');
+const socketIo = require('socket.io');
+const setupSocket = require('./socket/socket');
 const userRoute = require('./routes/userRoute');
+const chatRoute = require('./routes/chatRoute');
+const messageRoute = require('./routes/messageRoute');
 const postRoute = require('./routes/postRoute');
-const { checkLogin } = require('./utils/authHandler');
+const commentRoute = require('./routes/commentRoute');
+const reactionRoute = require('./routes/reactionRoute');
+const friendRequestRoute = require('./routes/friendRequestRoute');
+const notificationRoute = require('./routes/notificationRoute');
 const cookieParser = require('cookie-parser');
 
 const app = express();
@@ -25,7 +33,13 @@ app.use(express.json())
 
 // Mount existing routes
 app.use('/api/users', userRoute);
+app.use('/api/chats', chatRoute);
+app.use('/api/messages', messageRoute);
 app.use('/api/posts', postRoute);
+app.use('/api/comments', commentRoute);
+app.use('/api/reactions', reactionRoute);
+app.use('/api/friendRequests', friendRequestRoute);
+app.use('/api/notifications', notificationRoute);
 
 const port = process.env.PORT || 5000;
 const uri = process.env.ATLAS_URI;
@@ -34,15 +48,15 @@ app.get('/', (req, res, )=> {
   res.send('Hello World!');
 });
 
-server.listen(port, (req, res) => {
+server.listen(port, () => {
     console.log(`Server is running on port ${port}`)
 });
 
 // Setup Socket.io
 setupSocket(io);
 
-mongoose.connect(uri).then(()=>{
+mongoose.connect(uri).then(() => {
     console.log('MongoDB connected successfully')
-}).catch((err)=>{
+}).catch((err) => {
     console.log('MongoDB connection error: ', err)
-})
+});
